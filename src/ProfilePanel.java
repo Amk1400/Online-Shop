fc9ccd4a2f27c22cea1700f46488a85b3c31c63c
Amk1400 <kavoshamirmohammad@gmail.com>
2024-06-27 23:28:38 +0330

    [Feature] Sign Panel shows errors

--- src/ProfilePanel.java
+++ src/ProfilePanel.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.sql.SQLException;
 
 public class ProfilePanel extends AfterLoginPanel{
-    public ProfilePanel(JPanel lastPanel) throws SQLException {
+    public ProfilePanel(JPanel lastPanel, User user) throws SQLException {
         super(lastPanel);
     }
 }
